fe40df755d8f788a38e70cc93e8e12330e805428
aleem <sheikhaleem363@gmail.com>
2024-10-10 12:30:29 +0530

    Create the Food Site project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-red-300 text-2xl">Hello</div>
+    </>
+  );
+}
+
+export default App;
